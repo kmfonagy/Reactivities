4bbca99b5945547437ecde4cc56367f3176f71b5
Kristofer Fonagy <kristofer@fonagy.com>
2024-05-24 00:54:54 -0400

    Add Activities CRUD

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654b1f8435f1d988eeec532169282c0edc5938d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f0783e3930b40ed6533c388f820d037b47f808")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
