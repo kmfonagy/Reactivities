57918641dd43bad34eda3c8e4240a423fe3d071b
Kristofer Fonagy <kristofer@fonagy.com>
2024-07-13 19:21:36 -0400

    Add Client side Attendance

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0294d5ea07c231faa24718e6dbcfe92654ae150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae49c949c132ff2552232adc910f28c74b65a4ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
