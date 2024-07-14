2834bfb09b442a03ad329792f0b6a50951b10daa
Kristofer Fonagy <kristofer@fonagy.com>
2024-07-13 22:39:12 -0400

    Add API Image Upload

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae49c949c132ff2552232adc910f28c74b65a4ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57918641dd43bad34eda3c8e4240a423fe3d071b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
